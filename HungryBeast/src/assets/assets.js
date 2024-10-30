c9e215a9ed3a2566d0919c67e0a2b31c1c22669b
Chaithragna12 <aavetichaithragna@gmail.com>
2024-10-30 15:11:27 +0530

    chaithras menu first commit

--- HungryBeast/src/assets/assets.js
+++ HungryBeast/src/assets/assets.js
@@ -23,54 +23,60 @@ import pizzas from './Pizzas.jpg'
 import schezwan from './schezwan-chicken-fried-rice.jpg'
 import starter from './starter.jpg'
 import veg_starter_panner from './veg_starter_panner.jpg'
-import logo from './hglogo 1.png'
+// import logo from './hglogo 1.png'
+import menu_bg from './menu_bg.jpg'
+import chaithra from './chaithra.jpg'
+import cm from './cm.jpg'
+import cmenu from './cmenu.jpg'
+
+
 
 export const assets={
     bgimg,
     Chicken_Biryani_headers,familypack,
-    header_img,header,logo
+    header_img,header,menu_bg,chaithra,cm,cmenu
 }
 
 export const menu_list = [
     {
-        menu_name: "Veg Starters",
-        menu_image: starter
+        menu_image: starter,
+        menu_name: "Veg Starters"
     },
     {
-        menu_name: "Non-Veg Starters",
-        menu_image: nonvegstarter
+        menu_image: nonvegstarter,
+        menu_name: "Non-Veg Starters"
     },
     {
-        menu_name: "Main Course",
-        menu_image: mainCourse
+        menu_image: mainCourse,
+        menu_name: "Main Course"
     },
     {
-        menu_name: "Chef Special",
-        menu_image: chefSpecial
+        menu_image: chefSpecial,
+        menu_name: "Chef Special"
     },
     {
-        menu_name: "Fried Rice",
-        menu_image: chicken_fried_rice
+        menu_image: chicken_fried_rice,
+        menu_name: "Fried Rice"
     },
     {
-        menu_name: "Pizza",
-        menu_image: pizzas
+        menu_image: pizzas,
+        menu_name: "Pizza"
     },
     {
-        menu_name: "Momos",
-        menu_image:momos
+        menu_image:momos,
+        menu_name: "Momos"
     },
     {
-        menu_name: "Beverages",
-        menu_image: beverage
+        menu_image: beverage,
+        menu_name: "Beverages"
     },
     {
-        menu_name: "Ice Creams",
-        menu_image:icecreamdessert
+        menu_image:icecreamdessert,
+        menu_name: "Ice Creams"
     },
     {
-        menu_name: "Milk Shakes",
-        menu_image:milkshake
+        menu_image:milkshake,
+        menu_name: "Milk Shakes"
     }
 ]
 
--- HungryBeast/src/components/FullMenu/FullMenu.jsx
+++ HungryBeast/src/components/FullMenu/FullMenu.jsx
@@ -1,10 +1,41 @@
-// import React from 'react'
-import './fullmenu.css'
+import {MenuData } from '../../assets/assets';
+import './fullmenu.css'; 
 
-const FullMenu = () => {
+const Menu = () => {
   return (
-    <div>FullMenu</div>
-  )
-}
+    <div className="full-menu min-h-screen  p-6">
+    <h1 className="text-center text-4xl font-bold mb-10 text-yellow-400">Our Menu</h1>
 
-export default FullMenu
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-10">
+      {MenuData.map((menu) => (
+        <div
+          key={menu._id}
+          className=" p-6 flex flex-col items-center"
+        >
+          <h2 className="text-2xl font-semibold mb-4 text-yellow-400">{menu.title}</h2>
+          <div className='flex justify-center items-center gap-10'>
+          <img
+            src={menu.image}
+            alt={menu.title}
+             className="min-w-full h-60 object-cover"
+          />
+          <ul className="w-full">
+            {menu.items.map((item, index) => (
+              <li
+                key={index}
+                className="flex justify-between border-b border-gray-700 py-2"
+              >
+                <p className='w-40 font-semibold text-xl  text-white'>{item.name}</p>
+                <p className="font-semibold text-white">₹{item.price}</p>
+              </li>
+            ))}
+          </ul>
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>
+  );
+};
+
+export default Menu;
